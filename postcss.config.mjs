3868501f62b5fcc6c0e788f1d82b43f80ac68dde
Vajk93 <67112672+Vajk93@users.noreply.github.com>
2025-03-12 08:34:14 +0100

    save my work, i will continou with an another machine

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,9 @@
 const config = {
+  // content: [
+  //   "./app/**/*.{js,ts,jsx,tsx}",
+  //   "./pages/**/*.{js,ts,jsx,tsx}",
+  //   "./components/**/*.{js,ts,jsx,tsx}",
+  // ],
   plugins: {
     "@tailwindcss/postcss": {},
   },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,37 @@ export default function Home() {
 			width={0}
 			height={0}
 			sizes="100vw"
+			className="hidden md:block"
 			style={{ width: '100%', height: 'auto' }}         
 		/>
-		<h1 className="bg-test">is the background green? Then OK!</h1>
+		<Image
+			src="/images/bg-header-mobile.svg"
+			alt="Header background"
+			width={0}
+			height={0}
+			sizes="100vw"
+			className="md:hidden"
+			style={{ width: '100%', height: 'auto' }}         
+		/>
+
+
+
+
+
+
+
+		<div className="w-full max-w-[1110px] h-[152px] bg-white p-[26px]">
+jijiji
+		</div>
+
+
+
+
+
+
+
+
+
 	</div>
   );
 }
